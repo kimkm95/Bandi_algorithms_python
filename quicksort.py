1f13b9b08caa9eaf95844bc033a6599576c2e03b
kimkm95 <74487435+kimkm95@users.noreply.github.com>
2021-10-08 23:52:51 +0900

    sorting

---
+++ quicksort.py
@@ -0,0 +1,216 @@
+# Use a random number generator to fill up the given size of input array.
+# Count and show the number of comparison and exchange operations.
+# Have your own choice of the input data size.
+
+#2.1 The first element is the pivot as shown in the textbook.
+ 
+# import random
+
+# arr = []
+# for i in range (0,10):
+#     num = round(random.random()*100)
+#     arr.append(num)
+
+
+# out place sorting
+# def quick_sort(arr):
+#     if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+#         return arr
+    
+#     pivot, tail = arr[0], arr[1:] # pivot을 첫번째 숫자로 설정해 주고, tail 값으로 나머지 리스트 값을 준다.
+    
+#     left = [x for x in tail if x <= pivot] # left 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 작으면 left에 넣어준다.
+#     right = [x for x in tail if x > pivot] # right 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 크면 right에 넣어준다.
+    
+#     return quick_sort(left) + [pivot] + quick_sort(right) # 리턴 해준다.
+
+# arr = quick_sort(arr)
+
+# print(arr)
+
+
+# in place sorting
+# def partition(arr, start, end):
+#     pivot = arr[start] # 정렬하고자 하는 집합의 첫번째 원소를 피봇으로 한다.
+#     i = start + 1
+#     j = end
+#     while i <= j:
+#         while i <= j and arr[i] <= pivot: i += 1 # i 는 pivot 값보다 작은수면 계속해서 오른쪽으로 이동한다.
+#         while i <= j and arr[j] >= pivot: j -= 1 # j 는 pivot 값보다 큰수면 계속해서 왼쪽으로 이동한다.
+#         if i < j: # i와 j 가 이동할 수 없다면 그 값을 바꿔준다. 이런 경우를 i 와 j가 같아지는 지점에 이를 때까지 반복한다. 
+#             arr[i], arr[j] = arr[j], arr[i]
+#     arr[start], arr[j] = arr[j], arr[start] 
+#     return j # j 는 새로운 pivot 값이 된다.
+
+# def quicksort(arr, start, end):
+#     pivot = partition(arr, start, end) # 새로운 피봇값을 가운데 설정해 준다.
+#     if start < pivot-1:
+#         quicksort(arr, start, pivot-1) # 피봇값을 기준으로 재귀함수를 실행 해 준다.
+#     if end > pivot:
+#         quicksort(arr, pivot+1, end)
+
+# quicksort(arr, 0, len(arr)-1)
+# print(arr)
+
+
+# 2.2 A randomly chosen element between low and high is the pivot.
+# pivot_arr = [arr[0], arr[len(arr)- 1]]
+# pivot = random.choice(pivot_arr)
+
+# def quick_sort(arr):
+#     if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+#         return arr
+    
+#     pivot, tail = arr[0], arr[1:] # pivot을 첫번째 숫자로 설정해 주고, tail 값으로 나머지 리스트 값을 준다.
+    
+#     left = [x for x in tail if x <= pivot] # left 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 작으면 left에 넣어준다.
+#     right = [x for x in tail if x > pivot] # right 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 크면 right에 넣어준다.
+    
+#     return quick_sort(left) + [pivot] + quick_sort(right) # 리턴 해준다.
+
+# def quick_endsort(arr):
+#     if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+#         return arr
+    
+#     pivot, front = arr[len(arr) -1], arr[:len(arr) -1] # pivot을 마지막 숫자로 설정해 주고, front 값으로 나머지 리스트 값을 준다.
+    
+#     left = [x for x in front if x <= pivot] # left 라는 리스트를 만들어서 front리스트의 원소 값들을 pivot과 비교해 작으면 left에 넣어준다.
+#     right = [x for x in front if x > pivot] # right 라는 리스트를 만들어서 front리스트의 원소 값들을 pivot과 비교해 크면 right에 넣어준다.
+    
+#     return quick_endsort(left) + [pivot] + quick_endsort(right) # 리턴 해준다.
+
+# if(pivot == pivot_arr[0]):
+#     arr = quick_sort(arr)
+# else:
+#     arr = quick_endsort(arr)
+
+# print(arr)
+
+
+# 2.3 The pivot is chosen as the median number, as suggested by Robert Sedgewick, among the first, the last, 
+# and the mid elements between low and high. 
+# (For example, if the first, the mid, and the last element are 10, 7, 20, respectively, the first element 10 is the median value. In Korean, 중간값.)
+
+import random
+
+arr = []
+for i in range (0,10):
+    num = round(random.random()*100)
+    arr.append(num)
+
+potential_pivot = [arr[0],arr[len(arr)//2],arr[len(arr) -1]]
+sorted(potential_pivot)
+pivot = potential_pivot[1]
+
+def quick_startsort(arr):
+    if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+        return arr
+    
+    pivot, tail = arr[0], arr[1:] # pivot을 첫번째 숫자로 설정해 주고, tail 값으로 나머지 리스트 값을 준다.
+    
+    left = [x for x in tail if x <= pivot] # left 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 작으면 left에 넣어준다.
+    right = [x for x in tail if x > pivot] # right 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 크면 right에 넣어준다.
+    
+    return quick_startsort(left) + [pivot] + quick_startsort(right) # 리턴 해준다.
+
+def quick_endsort(arr):
+    if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+        return arr
+    
+    pivot, front = arr[len(arr) -1], arr[:len(arr) -1] # pivot을 마지막 숫자로 설정해 주고, front 값으로 나머지 리스트 값을 준다.
+    
+    left = [x for x in front if x <= pivot] # left 라는 리스트를 만들어서 front리스트의 원소 값들을 pivot과 비교해 작으면 left에 넣어준다.
+    right = [x for x in front if x > pivot] # right 라는 리스트를 만들어서 front리스트의 원소 값들을 pivot과 비교해 크면 right에 넣어준다.
+    
+    return quick_endsort(left) + [pivot] + quick_endsort(right) # 리턴 해준다.
+
+def quick_midsort(arr):
+    if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+        return arr
+    pivot = arr[len(arr) // 2] # 피봇을 정하기 위해 원소의 가장 가운데 값을 정한다.
+    low, equal, high = [], [], [] # 피봇을 기준으로 3가지 리스트를 만든다. 
+    for num in arr: # 함수내 원소를 한번씩 순회하여 피봇보다 크거나 작은지 판단한다.
+        if num < pivot:
+            low.append(num) # 작으면 less 
+        elif num > pivot:
+            high.append(num) # 크면 bigger
+        else:
+            equal.append(num) # 피봇과 같으면 equal 
+    return quick_midsort(low) + equal + quick_midsort(high) # 정렬된 함수를 리턴해준다.
+
+
+if(pivot == potential_pivot[0]):
+    arr = quick_startsort(arr)
+elif (pivot == potential_pivot[1]):
+    arr = quick_midsort(arr)
+else:
+    arr = quick_endsort(arr)
+
+print(arr)
+
+
+
+
+# def quick_sort(arr): # 퀵 정렬 함수 정의
+#     if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+#         return arr
+#     pivot = arr[len(arr) // 2] # 피봇을 정하기 위해 원소의 가장 가운데 값을 정한다.
+#     low, equal, high = [], [], [] # 피봇을 기준으로 3가지 리스트를 만든다. 
+#     for num in arr: # 함수내 원소를 한번씩 순회하여 피봇보다 크거나 작은지 판단한다.
+#         if num < pivot:
+#             low.append(num) # 작으면 less 
+#         elif num > pivot:
+#             high.append(num) # 크면 bigger
+#         else:
+#             equal.append(num) # 피봇과 같으면 equal 
+#     return quick_sort(low) + equal + quick_sort(high) # 정렬된 함수를 리턴해준다.
+
+
+# arr = [1,3,5,7,9,2,4,6,8]
+# arr = quick_sort(arr)
+
+# print(arr)
+
+
+
+# def quick_sort(arr):
+#     if len(arr) <= 1: # 정렬할 원소의 개수가 1보다 적다면 그냥 그 값 자체를 리턴해준다.
+#         return arr
+    
+#     pivot, tail = arr[0], arr[1:] # pivot을 첫번째 숫자로 설정해 주고, tail 값으로 나머지 리스트 값을 준다.
+    
+#     left = [x for x in tail if x <= pivot] # left 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 작으면 left에 넣어준다.
+#     right = [x for x in tail if x > pivot] # right 라는 리스트를 만들어서 tail리스트의 원소 값들을 pivot과 비교해 크면 right에 넣어준다.
+    
+#     return quick_sort(left) + [pivot] + quick_sort(right) # 리턴 해준다.
+
+# arr = [1,3,5,7,9,2,4,6,8]
+# arr = quick_sort(arr)
+
+# print(arr)
+
+
+# # 퀵 정렬 
+# def partition(arr, start, end):
+#     pivot = arr[start] # 정렬하고자 하는 집합의 첫번째 원소를 피봇으로 한다.
+#     i = start + 1
+#     j = end
+#     while i <= j:
+#         while i <= j and arr[i] <= pivot: i += 1 # i 는 pivot 값보다 작은수면 계속해서 오른쪽으로 이동한다.
+#         while i <= j and arr[j] >= pivot: j -= 1 # j 는 pivot 값보다 큰수면 계속해서 왼쪽으로 이동한다.
+#         if i < j: # i와 j 가 이동할 수 없다면 그 값을 바꿔준다. 이런 경우를 i 와 j가 같아지는 지점에 이를 때까지 반복한다. 
+#             arr[i], arr[j] = arr[j], arr[i]
+#     arr[start], arr[j] = arr[j], arr[start] 
+#     return j # j 는 새로운 pivot 값이 된다.
+
+# def quicksort(arr, start, end):
+#     pivot = partition(arr, start, end) # 새로운 피봇값을 가운데 설정해 준다.
+#     if start < pivot-1:
+#         quicksort(arr, start, pivot-1) # 피봇값을 기준으로 재귀함수를 실행 해 준다.
+#     if end > pivot:
+#         quicksort(arr, pivot+1, end)
+
+
+# arr = [1,3,5,7,9,2,4,6,8]
+# quicksort(arr, 0, len(arr)-1)
+# print(arr)
